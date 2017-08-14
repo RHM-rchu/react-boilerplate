c2a2aab1e70f68e4bfe7078a2095541b823f7ec1
Ron Chu <ron@remedyhealthmedia.com>
2017-08-14 07:22:36 -0400

    refactored forms, make more dynamic, remove redundancies

--- src/pages/ClinicEdit.js
+++ src/pages/ClinicEdit.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import _ from 'lodash'
 import { PageHeader, Form, FormGroup, Col, Button, FormControl, InputGroup, Glyphicon, HelpBlock } from 'react-bootstrap'
 import { Field, reduxForm } from 'redux-form'
 import { connect } from 'react-redux'
 import { validateEmail } from '../helpers.js'
 
 
+/**
+ *
+ * Define Form elelemnts and attribues for output
+ *
+ */
+
+const FIELDS = {
+  clinic: {
+    type: 'input',
+    label: 'Clinc Name',
+    id: 'clinicname',
+    placeholder: 'Clinicname',
+    validate: ['blank']
+  },
+  email: {
+    type: 'input',
+    label: 'email',
+    id: 'email',
+    placeholder: 'Email',
+    validate: ['blank', 'email'],
+    glyph: 'envelope'
+  },
+}
 
 /**
  *
@@ -13,22 +37,32 @@ import { validateEmail } from '../helpers.js'
  */
 class ClinicEdit extends React.Component {
   // add or edit form_data
-  form_type
+  // form_type
 
   constructor(props) {
     super(props)
     this.form_type = props.initialValues.clinicUid > 0 ? 'Edit' : 'Add'
   }
 
+
   render() {
+
+    const fieldsList = Object.keys(FIELDS).map(function(key, i) {
+      if(FIELDS[key].glyph) {
+        return <Field key={i} name={key} component={ClinicEdit.renderFieldsWGlyph}/>
+      } else {
+        return <Field key={i} name={key} component={ClinicEdit.renderFields}/>
+      }
+
+    })
+
     return (
       <div>
         <PageHeader>Clinic {this.form_type}</PageHeader>
         <Form horizontal>
-          <Field name="clinic" component={ClinicEdit.renderClinicName}/>
-          <Field name="email" component={ClinicEdit.renderEmail}/>
           <FormGroup>
             <Col smOffset={2} sm={8}>
+              {fieldsList}
               <Button type="submit" disabled={this.props.invalid || this.props.submitting}><Glyphicon glyph="floppy-disk"/> Save Clinic </Button>
             </Col>
           </FormGroup>
@@ -37,38 +71,36 @@ class ClinicEdit extends React.Component {
     )
   }
 
+
+
   /**
    *
-   * Render the clinic name form field
+   * Render a Generic form field
    *
    */
-  static renderClinicName(props) {
+  static renderFields(props) {
+    const fieldConfig = FIELDS[props.input.name]
     return (
       <FormGroup validationState={!props.meta.touched ? null : (props.meta.error ? 'error' : 'success')}>
-        <Col sm={2}>ClincName</Col>
+        <Col sm={3}>{fieldConfig.label}</Col>
         <Col sm={8}>
-          <FormControl {...props.input} id="clinicname" type="text" placeholder="Clinicname"/>
+          <FormControl {...props.input} id={fieldConfig.id} type={fieldConfig.type} placeholder={fieldConfig.placeholder}/>
           <FormControl.Feedback/>
           <HelpBlock>{props.meta.touched && props.meta.error ? props.meta.error : null}</HelpBlock>
         </Col>
       </FormGroup>
     )
   }
-
-  /**
-   *
-   * Render the email name form field
-   *
-   */
-  static renderEmail(props) {
+  static renderFieldsWGlyph(props) {
+    const fieldConfig = FIELDS[props.input.name]
     return (
-      <FormGroup>
-        <Col sm={2}>Email</Col>
+      <FormGroup validationState={!props.meta.touched ? null : (props.meta.error ? 'error' : 'success')}>
+        <Col sm={3}>{fieldConfig.label}</Col>
         <Col sm={8}>
           <InputGroup>
-            <FormControl {...props.input} id="email" type="text" placeholder="Email"/>
+            <FormControl {...props.input} id={fieldConfig.id} type={fieldConfig.type} placeholder={fieldConfig.placeholder}/>
             <InputGroup.Addon>
-              <Glyphicon glyph="envelope"/>
+              <Glyphicon glyph={fieldConfig.glyph}/>
             </InputGroup.Addon>
           </InputGroup>
           <FormControl.Feedback/>
@@ -78,23 +110,36 @@ class ClinicEdit extends React.Component {
     )
   }
 
+
+
 }
+
 
 
 
 // decorate component
 ClinicEdit = reduxForm({
   form: 'clinic_edit', //unique form name
+  fields: _.keys(FIELDS),
   validate: function(values) {
     let errors = {}
-    if(!values.email) {
-      errors.email = "Email is required"
-    } else if(! validateEmail(values.email)) {
-      errors.email = "Email format is invalid"
-    }
-    if(!values.clinic) {
-      errors.clinic = "Clinic Name is required"
-    }
+    _.each(FIELDS, (type, field) => {
+      _.each(FIELDS[field].validate, (check, key) => {
+        switch(check) {
+          case 'email':
+            if(! validateEmail(values[field])) {
+              return errors.email = `${field} format is invalid`
+            }
+            break
+          case 'blank':
+          default:
+            if(!values[field]) {
+              return errors[field] = `Enter a ${field}`
+            }
+            break
+        }
+      })
+    })
     return errors
   },
 }, null, null)(ClinicEdit)
@@ -106,11 +151,17 @@ function mapStateToProps(state, ownProps) {
     clinic: '',
     email: '',
   }
-  for(const clinic of state.theDatas.list) {
+
+  _.each(state.theDatas.list, (clinic, field) => {
     if(clinic.clinicUid === Number(ownProps.params.id)) {
       form_data = clinic
     }
-  }
+  })
+  // for(const clinic of state.theDatas.list) {
+  //   if(clinic.clinicUid === Number(ownProps.params.id)) {
+  //     form_data = clinic
+  //   }
+  // }
   return {
     initialValues: form_data
   }
